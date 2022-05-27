6983da5daf5b47afec4d7969201288be82cf05a3
Deyvison Tavares <deyvisontav@gmail.com>
2022-05-27 11:19:39 -0300

    primeiro commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+export function Form() {
+  const [email, setEmail] = useState()
+  const [senha, setSenha] = useState()
+
+  function handleSubmitFomr(e) {
+    e.preventDefault()
+    console.log(email, senha)
+  }
+
+  return (
+    <div  className='w-80  '>
+        <h2 className='text-black text-center text-base '>formulario</h2>
+        <br />
+      <form 
+      className='bg-zinc-900 shadow-md rounded-md p-4 space-y-4'
+      onSubmit={handleSubmitFomr}>
+        <div>
+          <label 
+          className='block text-base text-white mb-1'
+          htmlFor="email">Email:</label>
+          <input
+            className= ' bg-zinc-400 hover:bg-zinc-500 w-full  border-2 border-gray-800 focus:border-black rounded px-2 py-1 text-base text-white placeholder-zinc-100 focus:outline-none transition duration-150 ease-out hover:ease-in'
+            onChange={(e) => setEmail(e.target.value)}
+            type="email" id = 'email'
+            placeholder="digite eu email"
+          />
+        </div>
+        <div>
+          <label 
+          className='block text-base text-white mb-1'
+          htmlFor="password">Senha:</label>
+          <input
+            className= ' bg-zinc-400 hover:bg-zinc-500 w-full  border-2 border-gray-800 focus:border-black rounded px-2 py-1 text-base text-white placeholder-zinc-100 focus:outline-none transition duration-150 ease-out hover:ease-in'
+            onChange={(e) => setSenha(e.target.value)}
+            type="password" id='password'
+            placeholder="digite sua senha"
+          />
+        </div>
+        <div>
+          <button className='w-full text-sm bg-black hover:bg-gray-900 transition duration-150 ease-out hover:ease-in px-4 py-1 rounded text-white shadow ' 
+          onSubmit={handleSubmitFomr}>
+            enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
